b67726c66bdc2ddcfea668600186e2744c642343
Zahran Affan <muhammad.zahran51@ui.ac.id>
2026-09-21 19:59:47 +0700

    feat: Menambahkan fitur edit pada Experience

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_achievement, create_experience, get_experience_json, delete_experience
+from main.views import show_main, show_experience, show_achievement, create_experience, get_experience_json, delete_experience, update_experience
 
 app_name = "main"
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("experience/add/", create_experience, name="create_experience"),
     path("api/experience/", get_experience_json, name="get_experience_json"),
     path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    path("experience/<uuid:experience_id>/edit/", update_experience, name="update_experience"),
 ]
--- main/views.py
+++ main/views.py
@@ -52,6 +52,24 @@ def create_experience(request):
     }
     return render(request, "experience_form.html", context)
 
+def update_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    form = ExperienceForm(request.POST or None, instance=experience)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience berhasil diperbarui!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Muhammad Zahran Affan",
+        "form": form,
+        "experience": experience,
+    }
+
+    return render(request, "experience_form.html", context)
+
 def get_experience_json(request):
     title_query = request.GET.get("title", "").strip()
     experiences = Experience.objects.all()
